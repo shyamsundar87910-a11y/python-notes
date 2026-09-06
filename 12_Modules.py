8cbcdc08122fb679f442fcd3bda44198786c6ff5
Shyam <shyamsundar87910@gmail.com>
2026-09-06 08:04:28 +0530

    Add examples for math, random, and datetime modules

---
+++ 12_Modules.py
@@ -0,0 +1,67 @@
+# ===================== MODULES =====================
+
+# .........................Import Module.........................#
+
+import math
+
+print(math.sqrt(25))
+
+
+# .........................math Module.........................#
+
+import math
+
+print(math.pi)
+print(math.sqrt(16))
+print(math.pow(2, 3))
+print(math.ceil(4.2))
+print(math.floor(4.8))
+
+
+# .........................from import.........................#
+
+from math import sqrt
+
+print(sqrt(25))
+
+
+# .........................Multiple Import.........................#
+
+from math import sqrt, pi
+
+print(sqrt(16))
+print(pi)
+
+
+# .........................random Module.........................#
+
+import random
+
+print(random.randint(1, 10))
+
+
+# .........................random Choice.........................#
+
+import random
+
+numbers = [10, 20, 30, 40, 50]
+
+print(random.choice(numbers))
+
+
+# .........................datetime Module.........................#
+
+import datetime
+
+today = datetime.date.today()
+
+print(today)
+
+
+# .........................Current Date and Time.........................#
+
+import datetime
+
+now = datetime.datetime.now()
+
+print(now)
