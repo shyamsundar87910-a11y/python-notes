d5b1967b8ba0621ca57431a8b153936cdc39732c
Shyam <shyamsundar87910@gmail.com>
2026-09-13 08:03:15 +0530

    Create 44_VIRTUAL_ENVIRONMENT_AND_PIP.py for pip usage
    Added a Python script to demonstrate virtual environments and pip commands.

---
+++ 44_VIRTUAL_ENVIRONMENT_AND_PIP.py
@@ -0,0 +1,69 @@
+# ===================== 44_VIRTUAL_ENVIRONMENT_AND_PIP.py =====================
+
+
+# .........................Check Python Version.........................#
+
+import sys
+
+print(sys.version)
+
+
+# .........................Check Python Location.........................#
+
+import sys
+
+print(sys.executable)
+
+
+# .........................Install Package.........................#
+
+# Run this command in Terminal:
+# pip install pandas
+
+
+# .........................Install Multiple Packages.........................#
+
+# Run this command in Terminal:
+# pip install pandas numpy matplotlib
+
+
+# .........................Check Installed Packages.........................#
+
+# Run this command in Terminal:
+# pip list
+
+
+# .........................Show Package Information.........................#
+
+# Run this command in Terminal:
+# pip show pandas
+
+
+# .........................Upgrade Package.........................#
+
+# Run this command in Terminal:
+# pip install --upgrade pandas
+
+
+# .........................Uninstall Package.........................#
+
+# Run this command in Terminal:
+# pip uninstall pandas
+
+
+# .........................Create Virtual Environment.........................#
+
+# Run this command in Terminal:
+# python -m venv myenv
+
+
+# .........................Activate Virtual Environment.........................#
+
+# Windows:
+# myenv\Scripts\activate
+
+
+# .........................Deactivate Virtual Environment.........................#
+
+# Run this command in Terminal:
+# deactivate
